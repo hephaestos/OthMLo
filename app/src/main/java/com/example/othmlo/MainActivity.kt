76a1a3aee43070445b6b3e401d22e54546d9d627
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2021-03-27 15:36:37 -0400

    Added board size menu button and simple logic

--- app/src/main/java/com/example/othmlo/MainActivity.kt
+++ app/src/main/java/com/example/othmlo/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.othmlo
 
+import android.app.ActionBar
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    var boardSize: String? = null
+    var centerText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        centerText = findViewById(R.id.hello)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_board_size) {
+            createBoardDialog(this)
+            return true
+        }
+        return false
+    }
+
+    private fun createBoardDialog(context: Context) {
+        val textInputLayout = TextInputLayout(context)
+        val input = EditText(context)
+        input.inputType = InputType.TYPE_CLASS_NUMBER
+        textInputLayout.addView(input)
+
+        val builder = AlertDialog.Builder(context)
+            .setTitle("Enter Board Size")
+            .setView(textInputLayout)
+            .setMessage("Enter an integer 1-10")
+            .setPositiveButton("OK") { dialog, _ ->
+                if (input.text.toString().toInt() in 1..10) {
+                    boardSize = input.text.toString()
+                    centerText?.text = boardSize
+                    dialog.cancel()
+                } else {
+                    //input.text.clear()
+                }
+            }
+            .setNegativeButton("Cancel") {dialog, _ ->
+                dialog.cancel()
+            }.create()
+        builder.show()
     }
 }
